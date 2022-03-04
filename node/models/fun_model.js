9ebfa2238fd0c23101ae92b786a116cc895e34da
Bruno <brunocalopi@gmail.com>
2022-03-03 19:31:38 -0500

    cambios en models, se estructuró las funciones de la instancia películas

---
+++ node/models/fun_model.js
@@ -0,0 +1,32 @@
+const {ModeloPelícula} = require('./models');
+
+//La función para mostrar todas las instancias
+//select*from table
+
+const getAllMovies = async8req,res)=>{
+  try{
+    const Películas = await ModeloPelícula.findAll();
+    res.json(Películas);
+  } catch(error){
+    res.json({message: 'No se encontraron instancias'})
+  }
+}
+
+//La función para mostrar solo 1 instancia
+//sql : select*from table where id_película = 1;
+
+const getOneMovie = async(req,res)=>{
+  try{
+    const Película_unica = await ModeloPelícula.findAll({where: {id: req.params.id}});
+  } catch(error){
+    res.json({mensaje: "No se encontró la instancia"})
+  }
+}
+
+
+
+//La función para crear un registro
+
+//La función para actualizar un registro
+
+//La función para eliminar un registro (=> ModeloPelícula)
--- node/models/models.js
+++ node/models/models.js
@@ -6,5 +6,25 @@
 const {Sequelize, DataTypes} = require ('sequelize');
 const {database} = require('../database/db');
 
-console.log(database);
+//console.log(database);
 
+// generar una instancia del modelo.
+
+const ModeloPelícula = database.define('Película',{
+  title: {type: DataTypes.STRING},
+  content: {type: DataTypes.STRING}
+})
+
+console.log(ModeloPelícula);
+console.log(typeof(ModeloPelícula));
+
+
+module.exports.ModeloPelícula = ModeloPelícula
+
+
+/*
+En sql :
+create table database.tabla(
+  name varchar...
+)
+*/
